84cfd1aa4c029e3bea6dd39848edc1324db66cad
MichelChollet <michel.chollet@3ds.com>
2017-10-13 17:21:26 +0200

    Hello World Test

---
+++ HelloWorld/src/HelloWorldApp.java
@@ -0,0 +1,21 @@
+import java.io.File;
+import java.io.IOException;
+
+public class HelloWorldApp {
+
+	public static void main(String[] args) {
+		// Prints "Hello, World" to the terminal window.
+		System.out.println("Hello, World MM...");
+
+		/*try {
+			File file = new File("C:/tmp/myfile.txt");
+
+			if (file.createNewFile())
+				System.out.println("Success!");
+			else
+				System.out.println("Error, file already exists.");
+		} catch (IOException ioe) {
+			ioe.printStackTrace();
+		}*/
+	}
+}
